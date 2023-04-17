a533c7ed568321afca177e02f0571299ccb9b70b
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-04-17 16:57:46 +0200

    feat: exercise done

--- index.php
+++ index.php
@@ -7,8 +7,41 @@
     <title>PHP-Badwords-Redirect</title>
 </head>
 <body>
-    <header></header>
-    <main></main>
+    <header>
+    <h1>Redirected Page</h1>
+    </header>
+    <main>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];    
+
+        $message = $_POST["message"];         
+        $message_length = strlen($message);
+        //$name_lower = strtolower($name);
+
+        // The str_ireplace function performs a case-insensitive string replacement. 
+        $modified_message = str_ireplace($name, "***", $message);
+        
+        
+        echo "<p>Your message is: $message. <br><br> The Word you want to censor is: '$name'</p>";
+        echo "<br>";
+        echo "<p>Your message is $message_length characters long.</p>";
+        echo "<br>";        
+        echo "<h4>Modified message</h4>";
+        echo $modified_message;
+        
+        
+        
+
+        
+        
+        }
+    ?>
+    
+        
+
+
+    </main>
     <footer></footer>
     
 </body>
